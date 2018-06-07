d044b0ba9328e38baa66e5ac1267cc6b06adcf02
Willem van Klinken <willem.v.klinken@gmail.com>
2018-06-07 21:18:52 +0200

    Add figaro - Protect my secret data in config/application.yml

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,35 @@
+class DosesController < ApplicationController
+  def new
+    @dose = Dose.new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @ingredients = Ingredient.all
+  end
+
+  def create
+    @dose = Dose.new(dose_params)
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose.cocktail = @cocktail
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      @ingredients = Ingredient.all
+      # render 'new'
+      render 'cocktails/show'
+      #redirect_to :back
+    end
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @cocktail = @dose.cocktail
+
+    @dose.destroy
+    redirect_to cocktail_path(@cocktail)
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+require "open-uri"
+require 'json'
+require 'faker'
+
+puts "clearing the field (destroy all records)"
+
+# Dose.destroy_all
+Ingredient.destroy_all
+# Cocktail.destroy_all
+
+puts "planting the seeds (seeding database)"
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+attr_serialized = open(url).string
+attributes = JSON.parse(attr_serialized)
+
+attributes['drinks'].each {|data|
+  Ingredient.create(name: data['strIngredient1'] )
+}
+
+puts "seeded ingredients"
+
